eac5eba0cabe59a7bbe075f73dc98db1f189a93c
suny-am <visualarea.1@gmail.com>
2024-05-24 12:42:55 +0000

    add identity server templates

---
+++ Tournaments.Client/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using IdentityModel.Client;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
+
+var client = new HttpClient();
+
+var discovery = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
+
+if (discovery.IsError)
+{
+    Log.Error(discovery.Error!);
+    Log.Error(discovery.Exception!.InnerException!.Message);
+    return;
+}
+
+// var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+// {
+//     Address = discovery.TokenEndpoint,
+//     ClientId = "devClient",
+//     ClientSecret = "devSecret",
+//     Scope = "tournamentAPI"
+// });
+
+// if (tokenResponse.IsError)
+// {
+//     Log.Error(tokenResponse.Error!);
+//     Log.Error(tokenResponse.ErrorDescription!);
+//     Log.Error(JsonSerializer.Serialize(tokenResponse.ErrorType, JsonWriteOptions()));
+//     return;
+// }
+
+// var apiClient = new HttpClient();
+
+// apiClient.SetBearerToken(tokenResponse.AccessToken!);
+
+// var response = await apiClient.GetAsync("https://localhost:3000/games");
+
+// if (!response.IsSuccessStatusCode)
+// {
+//     Log.Information(response.StatusCode.ToString());
+// }
+// else
+// {
+//     var doc = JsonDocument.Parse(await response.Content.ReadAsStringAsync());
+
+//     Log.Error(JsonSerializer.Serialize(doc, JsonWriteOptions()));
+// }
---
+++ Tournaments.IdentityServer/Config.cs
@@ -0,0 +1,40 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace Tournaments.IdentityServer;
+
+public static class Config
+{
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new IdentityResource[]
+        {
+            new IdentityResources.OpenId()
+        };
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+    new ApiScope[]
+    {
+        new ApiScope(name: "tournamentAPI", displayName: "Tournaments API")
+    };
+
+    public static IEnumerable<Client> Clients =>
+    new Client[]
+
+    {
+        new Client
+        {
+            ClientId = "devClient",
+
+            // no interactive user, use the clientid/secret for authentication
+            AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+            // secret for authentication
+            ClientSecrets =
+            {
+                new Secret("devSecret".Sha256())
+            },
+
+            // scopes that client has access to
+            AllowedScopes = { "tournamentAPI" }
+        }
+    };
+}
